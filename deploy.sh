662c7b64bbadd291c34e5a796bfe7fc198d75c4b
Faraz Maschhur <f.maschhur@live.de>
2022-01-18 15:20:22 +0100

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,7 @@ kubectl apply -f kubernetes_config/namespace/namespace.yaml
 kubectl apply -f kubernetes_config/tapira_db/mongodb_config.yaml
 kubectl apply -f kubernetes_config/tapira_db/mongodb_svc.yaml
 kubectl apply -f kubernetes_config/tapira_db/mongodb_pod.yaml
+kubectl apply -f kubernetes_config/postgrest/postgrest_svc.yaml
 kubectl apply -f kubernetes_config/postgrest/postgrest_pod.yaml
 kubectl apply -f kubernetes_config/secrets/tapira-secrets.yaml
 kubectl apply -f kubernetes_config/tapira/tapira_pod.yaml
