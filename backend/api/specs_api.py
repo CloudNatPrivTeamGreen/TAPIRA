110838bd12ed6037612133200b63ce037a4a7ee2
Ivan Nikolov <ivalerievn@gmail.com>
2022-02-07 14:51:25 +0100

    Fix import of models

--- backend/api/specs_api.py
+++ backend/api/specs_api.py
@@ -7,7 +7,7 @@ from flask.views import MethodView
 from flask_smorest import Blueprint, abort
 
 from backend import config as config
-from backend import models as models
+from backend.models import models
 from backend.schema import schema
 from backend.services import collection_service as s
 
