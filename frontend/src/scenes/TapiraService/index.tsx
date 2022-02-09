bbb976a644303f7a35a25035c580a2b1452fc0b8
ahmadm <mugheera.ahmad@adesso.de>
2022-02-09 13:54:13 +0100

    using react fragment

--- frontend/src/scenes/TapiraService/index.tsx
+++ frontend/src/scenes/TapiraService/index.tsx
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import { inject, observer } from "mobx-react";
 import { Stores } from "../../stores/storeIdentifier";
 import TapiraApiStore from "../../stores/tapiraApiStore";
-import { RoutingParameters } from "../../components/Router/router.config";
 
 const TapiraService = (props: any) => {
   const { tapiraApiStore }: { [Stores.TapiraApiStore]: TapiraApiStore } = props;
@@ -12,10 +11,6 @@ const TapiraService = (props: any) => {
 
   const getSpecifications = useCallback(async () => {
     await tapiraApiStore.getSpecificationsForService(serviceName as string);
-    console.log(
-      "in Tapira Service component: ",
-      tapiraApiStore.serviceSpecifications
-    );
   }, []);
 
   useEffect(() => {
@@ -23,7 +18,7 @@ const TapiraService = (props: any) => {
   }, []);
 
   return (
-    <>
+    <React.Fragment>
       <h1>Is the Tapira Service</h1>
       <ul>
         <li>An item 1</li>
@@ -46,7 +41,7 @@ const TapiraService = (props: any) => {
         <li>An item 1</li>
         <li>An Item 2</li>
       </ul>
-    </>
+    </React.Fragment>
   );
 };
 
