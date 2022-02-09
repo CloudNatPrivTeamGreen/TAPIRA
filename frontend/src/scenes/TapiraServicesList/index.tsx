413318314c7c26d1e7a06f77332355f1dc48251d
ahmadm <mugheera.ahmad@adesso.de>
2022-02-09 19:47:32 +0100

    added collapse panels

--- frontend/src/scenes/TapiraServicesList/index.tsx
+++ frontend/src/scenes/TapiraServicesList/index.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useCallback, useState } from "react";
 import { Link } from "react-router-dom";
-import { List } from "antd";
+import { Collapse, Typography, Button } from "antd";
 import { inject, observer } from "mobx-react";
 import { Stores } from "../../stores/storeIdentifier";
 import TapiraApiStore from "../../stores/tapiraApiStore";
@@ -9,45 +9,57 @@ import {
   RoutingParameters,
 } from "../../components/Router/router.config";
 
+const { Title } = Typography;
+const { Panel } = Collapse;
+
+const PanelHeader = ({ serviceName }) => {
+  return (
+    <React.Fragment>
+      <Button style={{ marginRight: "10px" }}>Upload</Button>
+      <Button style={{ marginRight: "10px" }}>Compare</Button>
+      <Button style={{ marginRight: "10px" }}>Evolution</Button>
+    </React.Fragment>
+  );
+};
+
 const TapiraServicesList = (props: any) => {
   const { tapiraApiStore }: { [Stores.TapiraApiStore]: TapiraApiStore } = props;
-  const [loading, setLoading] = useState(true);
-  const [services, setServices] = useState(new Array<string>());
+  const [services, setServices] = useState<Array<string>>(new Array<string>());
+  const [openPanelIndex, setOpenPanelIndex] = useState<string | null>(null);
 
   const getApiClaritySpecs = useCallback(async () => {
-    setLoading(true);
     await tapiraApiStore.getApiclaritySpecs();
     setServices(tapiraApiStore.apiClaritySpecs);
-    setLoading(false);
   }, [tapiraApiStore]);
 
   useEffect(() => {
     getApiClaritySpecs();
   }, [getApiClaritySpecs]);
 
+  const onCollapseCallback = (key: string | string[]) => {
+    if (typeof key === "object") {
+      return;
+    }
+    const index = parseInt(key);
+    setOpenPanelIndex(services[index]);
+  };
+
+  const panels = services.map((service: string, index: number) => (
+    <Panel
+      header={<span style={{ fontSize: "20px" }}>{service}</span>}
+      extra={<PanelHeader serviceName={service} />}
+      key={index}
+    >
+      <p>service</p>
+    </Panel>
+  ));
+
   return (
     <React.Fragment>
-      <List
-        loading={loading}
-        itemLayout="horizontal"
-        dataSource={services}
-        renderItem={(item) => (
-          <List.Item
-            actions={[
-              <Link
-                to={RoutePaths.Service.toString().replace(
-                  `${RoutingParameters.ServiceName.toString()}`,
-                  item
-                )}
-              >
-                <span>Show API Specs</span>
-              </Link>,
-            ]}
-          >
-            <List.Item.Meta title={item} />
-          </List.Item>
-        )}
-      />
+      <Title>Services</Title>
+      <div className="content">
+        <Collapse onChange={onCollapseCallback}>{panels}</Collapse>
+      </div>
     </React.Fragment>
   );
 };
