d69265c4fe0cb77c28debbcf52e363489a55e0b0
ahmadm <mugheera.ahmad@adesso.de>
2022-02-14 14:16:05 +0100

    added ApiDiffView to the evolution page

--- frontend/src/scenes/EvolutionPage/index.tsx
+++ frontend/src/scenes/EvolutionPage/index.tsx
@@ -6,6 +6,8 @@ import { inject, observer } from 'mobx-react';
 import { Select, Typography, Row, Col } from 'antd';
 import { Stores } from '../../stores/storeIdentifier';
 import TapiraApiSpecificationsStore from '../../stores/tapiraApiSpecificationsStore';
+import TapiraApiComparisonStore from '../../stores/tapiraApiComparisonStore';
+import ApiDiffView from '../../components/PartialComponents/ApiDiffView';
 
 const { Title } = Typography;
 const { Option } = Select;
@@ -30,12 +32,16 @@ function createVersionOptions(
 
 const EvolutionPage = ({
   tapiraApiSpecificationsStore,
+  tapiraApiComparisonStore,
 }: {
   [Stores.TapiraApiSpecificationsStore]?: TapiraApiSpecificationsStore;
+  [Stores.TapiraApiComparisonStore]?: TapiraApiComparisonStore;
 }) => {
   const { serviceName } = useParams();
   const [versionListA, setVersionListA] = useState<IVersionOption[]>();
   const [versionListB, setVersionListB] = useState<IVersionOption[]>();
+  const [selectedA, setSelectedA] = useState<string>();
+  const [selectedB, setSelectedB] = useState<string>();
 
   const createCallbackForSpecVersions = useCallback(async () => {
     if (!!serviceName) {
@@ -53,9 +59,38 @@ const EvolutionPage = ({
     }
   }, [serviceName, tapiraApiSpecificationsStore]);
 
+  const createCallbackForEvolution = useCallback(async () => {
+    if (selectedA && selectedB && serviceName) {
+      await tapiraApiComparisonStore?.getEvolutionForService(
+        serviceName,
+        selectedA,
+        selectedB
+      );
+    }
+  }, [selectedA, selectedB, serviceName, tapiraApiComparisonStore]);
+
   useEffect(() => {
     createCallbackForSpecVersions();
-  }, [createCallbackForSpecVersions]);
+    createCallbackForEvolution();
+  }, [createCallbackForEvolution, createCallbackForSpecVersions]);
+
+  const onChangeListA = (value: string) => {
+    if (tapiraApiSpecificationsStore?.versionTags) {
+      setVersionListB(
+        createVersionOptions(tapiraApiSpecificationsStore?.versionTags, value)
+      );
+    }
+    setSelectedA(value);
+  };
+
+  const onChangeListB = (value: string) => {
+    if (tapiraApiSpecificationsStore?.versionTags) {
+      setVersionListA(
+        createVersionOptions(tapiraApiSpecificationsStore.versionTags, value)
+      );
+    }
+    setSelectedB(value);
+  };
 
   const optionsListA = versionListA?.map((option: IVersionOption) => (
     <Option key={`${option.value}_A`} {...option}>
@@ -76,22 +111,38 @@ const EvolutionPage = ({
         <Row>
           <Col span={8} offset={2}>
             <Title level={4}>Version B</Title>
-            <Select style={{ width: 180 }} allowClear>
+            <Select
+              placeholder="select a version"
+              style={{ width: 180 }}
+              allowClear
+              onChange={onChangeListA}
+            >
               {optionsListA}
             </Select>
           </Col>
           <Col span={8} offset={4}>
             <Title level={4}>Version B</Title>
-            <Select style={{ width: 180 }} allowClear>
+            <Select
+              placeholder="select version to compare"
+              style={{ width: 180 }}
+              allowClear
+              onChange={onChangeListB}
+            >
               {optionsListB}
             </Select>
           </Col>
         </Row>
+        {!!tapiraApiComparisonStore?.evolutionResponse && (
+          <div className="specs-evolution__diffs">
+            <ApiDiffView {...tapiraApiComparisonStore?.evolutionResponse} />
+          </div>
+        )}
       </div>
     </React.Fragment>
   );
 };
 
-export default inject(Stores.TapiraApiSpecificationsStore)(
-  observer(EvolutionPage)
-);
+export default inject(
+  Stores.TapiraApiSpecificationsStore,
+  Stores.TapiraApiComparisonStore
+)(observer(EvolutionPage));
