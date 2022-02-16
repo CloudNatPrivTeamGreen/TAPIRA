501ff757f232bb5b610259b8acb4f1b5de689819
ahmadm <mugheera.ahmad@adesso.de>
2022-02-16 10:12:53 +0100

    added key to the list items

--- frontend/src/scenes/ConflictsPage/index.tsx
+++ frontend/src/scenes/ConflictsPage/index.tsx
@@ -12,48 +12,45 @@ import './index.scss';
 
 const { Title } = Typography;
 
-
 const ConflictsPage = ({
   tapiraApiProposalsStore,
 }: {
   [Stores.TapiraApiProposalsStore]: TapiraApiProposalsStore;
 }) => {
-
-  const [allConflictsList, setAllConflictsList] = useState<Array<string>>(new Array<string>());
+  const [allConflictsList, setAllConflictsList] = useState<Array<string>>(
+    new Array<string>()
+  );
 
   const getAllConflicts = useCallback(async () => {
     await tapiraApiProposalsStore.getAllConflicts();
-    setAllConflictsList(
-      tapiraApiProposalsStore.allConflictsList
-    );
+    setAllConflictsList(tapiraApiProposalsStore.allConflictsList);
   }, [tapiraApiProposalsStore]);
 
   useEffect(() => {
     getAllConflicts();
   }, [getAllConflicts]);
 
+  const conflictList = allConflictsList.map(
+    (conflict: string, index: number) => (
+      <List.Item key={index + conflict}>
+        <Link
+          to={RoutePaths.CompareSpecs.replace(
+            RoutingParameters.ServiceName,
+            conflict
+          ).replace(RoutingParameters.Version, conflict)}
+        >
+          {conflict}
+        </Link>
+      </List.Item>
+    )
+  );
 
-  const conflictList = allConflictsList.map((conflict: string, index: number) => (
-    
-    <List.Item>
-      <Link to=
-        {RoutePaths.CompareSpecs.replace(
-          RoutingParameters.ServiceName, conflict
-        ).replace(RoutingParameters.Version, conflict)
-      }>
-        {conflict}
-      </Link>
-    </List.Item>
-
-  ));
-
-  return <React.Fragment>
-    <Title> Conflicts </Title>
-    <div className="content conflicts-list">
-      {conflictList}
-    </div>
-  </React.Fragment>;
-
+  return (
+    <React.Fragment>
+      <Title> Conflicts </Title>
+      <div className="content conflicts-list">{conflictList}</div>
+    </React.Fragment>
+  );
 };
 
 export default inject(Stores.TapiraApiProposalsStore)(observer(ConflictsPage));
