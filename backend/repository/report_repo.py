45422f4dac16c731c88cf10113299ffd19991767
mariowoj99 <supermario.woj@gmail.com>
2022-03-01 20:04:12 +0100

    Implemented basic report_repo for report collection

---
+++ backend/repository/report_repo.py
@@ -0,0 +1,30 @@
+import datetime
+
+from flask import g
+from flask_pymongo import PyMongo
+
+from backend import app
+from backend.models.models import ApiDiffs
+
+
+def get_db():
+    if 'db' not in g:
+        mongodb_client = PyMongo(app)
+        g.db = mongodb_client.db
+
+    return g.db
+
+def create_report(report, start_timestamp, end_timestamp):
+    return get_db().reports.insert_one({"report": report, "start_timestamp": start_timestamp, "end_timestamp": end_timestamp})
+
+def find_report_by_timestamps(start_timestamp, end_timestamp):
+    return get_db().reports.find_one({"start_timestamp": start_timestamp, "end_timestamp": end_timestamp})
+
+def delete_report_by_timestamps(start_timestamp, end_timestamp):
+    deleted = get_db().reports.delete_one({"start_timestamp": start_timestamp, "end_timestamp": end_timestamp})
+
+    if deleted.deleted_count == 1:
+        print(f'Deleted report for timestamps: {start_timestamp} and {end_timestamp}')
+
+def find_all_reports():
+    return get_db().reports.find({})
--- backend/services/report_service.py
+++ backend/services/report_service.py
@@ -1,11 +1,17 @@
+from backend.repository import report_repo
+
 def get_report_by_timestamps(start_timestamp, end_timestamp):
+    print(report_repo.find_report_by_timestamps(start_timestamp,end_timestamp))
     return {"report": {}, "start_timestamp": start_timestamp, "end_timestamp": end_timestamp}
     
 def create_report_with_timestamps(start_timestamp, end_timestamp):
+    print(report_repo.create_report({"Test":"1234"},start_timestamp,end_timestamp))
     return {"report": {}, "start_timestamp": start_timestamp, "end_timestamp": end_timestamp}
 
 def delete_report_by_timestamps(start_timestamp, end_timestamp):
+    report_repo.delete_report_by_timestamps(start_timestamp,end_timestamp)
     return "Report was deleted for time period of " + start_timestamp + "and" + end_timestamp
 
 def get_all_reports():
+    print(report_repo.find_all_reports())
     return [{"report": {}, "start_timestamp":"MOCK", "end_timestamp": "MOCK"}]
