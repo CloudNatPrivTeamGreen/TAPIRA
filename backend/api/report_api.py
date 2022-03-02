603d346ddbfeca668d3c9d496930f40e86ced100
Ivan Nikolov <ivalerievn@gmail.com>
2022-03-02 15:52:37 +0100

    Give tapira access to listing pods and nodes resources
    Service Account + Role + Role Binding

--- backend/api/report_api.py
+++ backend/api/report_api.py
@@ -9,6 +9,7 @@ from kubernetes import client, config as kub_config
 from backend.models import models
 from backend.schema import schema
 from backend.services import collection_service as s
+from flask import jsonify
 
 ##################################################
 # Variable Definition
@@ -24,14 +25,24 @@ class ApiServerTest(MethodView):
         """Fetch provided and reconstructed specifications from APIClarity and save them in database.
         ---
         """
-        kub_config.load_incluster_config()
+        print("test")
+        kub_config.load_config()
 
+        print("test1")
         v1 = client.CoreV1Api()
         print("Listing pods with their IPs:")
-        ret = v1.list_pod_for_all_namespaces(watch=False)
-        for i in ret.items:
+        pods = v1.list_namespaced_pod("sock-shop", watch=False)
+        for i in pods.items:
             print("%s\t%s\t%s" % (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
+        print(f'First pod:{pods.items[0]}')
 
-        return {"pods": ret}
+        print("Listing nodes with their IPs:")
+        nodes = v1.list_node()
+        for i in nodes.items:
+            print(f'{i.metadata.annotations}')
+
+        pods = v1.api_client.sanitize_for_serialization(pods)
+        nodes = v1.api_client.sanitize_for_serialization(nodes)
+        return jsonify({"nodes": nodes, "pods": pods})
 
 
